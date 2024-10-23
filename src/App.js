bfcdc009b3ca4d11bf7ba5308f36cb6a11889d4e
Mohammadananta <anantamohammad2003@gmail.com>
2024-10-23 11:42:41 +0700

    pe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import "./css/App.js";
+import ".App.css";
 
 Chart.register(...registerables);
 
